d27717efef33324e6359ff392f3ca8b7f53dac53
Jenny Chang <jennychang@Jennys-MacBook-Pro-2.local>
2019-02-11 20:31:36 -0800

    added tables

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,25 @@
 SELECT 'hello, SQLite';
+CREATE TABLE fleet (id INTEGER PRIMARY KEY);
+
+CREATE TABLE ship (
+id INTEGER PRIMARY KEY, 
+dateBuilt TEXT, 
+fleetID INTEGER,
+FOREIGN LEY(fleetID) REFERENCES fleet(id)
+);
+
+CREATE TABLE sailor (
+id INTEGER PRIMARY KEY, 
+dateOfBirth TEXT
+);
+
+CREATE TABLE duty (
+shipID INTEGER,
+sailorID INTEGER,
+rank INTEGER, 
+startDate TEXT, 
+endDate TEXT,
+FOREIGN KEY(shipID) REFERENCES ship(id), 
+FOREIGN KEY(sailorID) REFERENCES sailor(id)
+);
+
